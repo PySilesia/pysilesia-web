155a8807cef8955c342f0c3c121be58723da291e
Jakub Wiśniowski <restless.being@gmail.com>
2013-11-12 08:42:22 +0100

    sitemedia url

--- project/settings.py
+++ project/settings.py
@@ -209,7 +209,7 @@ CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/static/"
+STATIC_URL = "/site_media/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
